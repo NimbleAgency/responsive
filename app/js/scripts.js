b992c6a0ae28828a740925678a97d964b9f58f3f
Jeff Nordstedt <jeff@nimbleagency.com>
2017-03-12 18:25:02 -0400

    Creating a Tween Animation

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -6,5 +6,23 @@ $(function() {
   $(window).resize(function() {
     var wheight = $(window).height(); // get height of window
     $('.fullheight').css('height', wheight);
-  })
-});
+  })  // on resize
+
+
+  // set up Scroll Magic
+
+  var controller = new ScrollMagic({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
+
+  var attractionstween = TweenMax.staggerFromTo('#attractions article', 1, { opacity:0, scale: 0}, {delay: 0.3, opacity: 1, scale: 1, ease: Back.easeOut});
+
+  var scene = new ScrollScene({
+    triggerElement: '#attractions'
+  }).setTween(attractionstween).addTo(controller);
+
+
+
+}); // on load
